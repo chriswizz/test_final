8cf022f091f29448b9ba71a41a371ce4cca5473b
BabSchu <you@example.com>
2019-12-13 13:48:45 +0100

    add crud project-folder

---
+++ creating-object-oriented-crud-system-using-php-and-mysql/Components/footer.php
@@ -0,0 +1,38 @@
+<footer class="page-footer mt-4 py-4 px-0 sticky-bottom">
+    <div class="row m-0">
+        <div class="col-12 text-center col-md-4 text-md-right p-0">
+            <p class="mt-2"> @Copyright 2019 | Barbara Schuch </p>
+        </div>
+        <div class="col-12 col-md-4 mt-2 text-center p-0">
+            <div class="socialmedia-icon">
+                <a href="#"><i class="fab fa-twitter"></i></a>
+            </div>
+            <div class="socialmedia-icon">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+            </div>
+            <div class="socialmedia-icon">
+                <a href="#"><i class="fab fa-google-plus-g"></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--JQuery<--->
+<!-- <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script> -->
+<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+crossorigin="anonymous"></script> -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<!--popper.js--->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+crossorigin="anonymous"></script>
+<!--bootstrap.js--->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+crossorigin="anonymous"></script>
+<!---JS--->
+<!-- <script type="text/javascript" src="../JS/script.js"></script> -->
+<script type="text/javascript" src="../JS/script-ajax.js" charset="UTF-8"></script>
+</body>
+
+</html>
