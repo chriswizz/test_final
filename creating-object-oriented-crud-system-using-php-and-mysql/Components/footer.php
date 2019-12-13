792b5e1ec0b413c939f0378c34c23bb942d4916e
BabSchu <you@example.com>
2019-12-13 14:42:34 +0100

    add admin-panel

--- creating-object-oriented-crud-system-using-php-and-mysql/Components/footer.php
+++ creating-object-oriented-crud-system-using-php-and-mysql/Components/footer.php
@@ -1,7 +1,7 @@
 <footer class="page-footer mt-4 py-4 px-0 sticky-bottom">
     <div class="row m-0">
         <div class="col-12 text-center col-md-4 text-md-right p-0">
-            <p class="mt-2"> @Copyright 2019 | Barbara Schuch </p>
+            <p class="mt-2"> @Copyright 2019 | BlueBearCoders </p>
         </div>
         <div class="col-12 col-md-4 mt-2 text-center p-0">
             <div class="socialmedia-icon">
@@ -17,10 +17,6 @@
     </div>
 </footer>
 <!--JQuery<--->
-<!-- <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script> -->
-<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-crossorigin="anonymous"></script> -->
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <!--popper.js--->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
@@ -32,7 +28,6 @@ integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07j
 crossorigin="anonymous"></script>
 <!---JS--->
 <!-- <script type="text/javascript" src="../JS/script.js"></script> -->
-<script type="text/javascript" src="../JS/script-ajax.js" charset="UTF-8"></script>
 </body>
 
 </html>
---
+++ creating-object-oriented-crud-system-using-php-and-mysql/admin-panel.php
@@ -0,0 +1,53 @@
+<?php
+include_once "Components/head.php";
+include_once 'CrudController.php';
+$crudcontroller = new CrudController();
+$result = $crudcontroller->readData();
+?>
+
+<h1>Adminpanel</h1>
+
+
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Image</th>
+      <th scope="col">ID</th>
+      <th scope="col">Title</th>
+      <th scope="col">Description</th>
+      <th scope="col">Show</th>
+      <th scope="col">Update Course</th>
+      <th scope="col">Update Course Item</th>
+    </tr>
+  </thead>
+  <tbody>
+<?php
+  if (! empty($result)) {
+    foreach ($result as $k => $v) {
+?>
+    <tr>
+        <td><img src="<?php echo $result[$k]["image"]; ?>" alt=""></td>
+        <td><?php echo $result[$k]["course_id"]; ?></td>
+        <td><?php echo $result[$k]["title"]; ?></td>
+        <td><?php echo $result[$k]["description"]; ?></td>
+        <td><?php echo $result[$k]["active"]; ?></td>
+        <td>
+            <button class="btn btn-danger bn-edit" id="<?php echo $result[$k]["id"]; ?>">Edit</button>
+        </td>
+        <td>
+            <button class="btn btn-danger bn-action" id="<?php echo $result[$k]["id"]; ?>">Edit</button>
+        </td>
+
+    </tr>
+    <?php
+    }
+}
+?>
+  </tbody>
+</table>
+
+
+
+<?php
+include_once "Components/footer.php";
+?>
