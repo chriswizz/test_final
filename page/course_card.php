2ec5850901fa2d39481bcaba7cbf200bedc48d2c
BabSchu <you@example.com>
2019-12-16 16:54:56 +0100

    add price to courseCard

--- page/course_card.php
+++ page/course_card.php
@@ -2,21 +2,48 @@
 if (! empty($result)) {
     foreach ($result as $k => $v) {
         ?>
-<div class="box-container">
+<!-- <div class="box-container">
     <div class="title">
         <?php echo $result[$k]["title"]; ?>
     </div>
     <div class="description">
         <?php echo $result[$k]["description"]; ?>...
     </div>
-    <!-- <div class="price">
+    <div class="price">
         <?php echo $result[$k]["price"]; ?>
-    </div> -->
-    <div class="action">
-        <button class="btn-action showBtn"
-            id="<?php echo $result[$k]["course_id"]; ?>">Show</button>
     </div>
-</div>
+    <div class="action">
+        <button class="btn-action showBtn" id="<?php echo $result[$k]["course_id"]; ?>">Show</button>
+    </div>
+</div> -->
+
+<!-- Ching -->
+
+    <div class="card col-md-4 store-item ">
+        <!-- <img class="card-img-top" src="<?php echo $result[$k]["image"]; ?>" alt="Card image cap"> -->
+        <img class="card-img-top" src="https://codefactory.wien/wp-content/themes/hestia/cf-images/big-jpg/2-fullstack.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title"><?php echo $result[$k]["title"]; ?></h5>
+            <p class="card-text">&euro; <?php echo implode($crudcontroller->readCoursePrice($result[$k]["course_id"])[0])/100; ?></p>
+            <div class="row">
+                <div class="col-md-6">
+                    <select class="md-form mdb-select colorful-select dropdown-primary">
+                    <option value="" disabled selected>Choose your section</option>
+                    <option value="1">14/01/2020-30/04/2020</option>
+                    <option value="2">14/02/2020-30/05/2020</option>
+                    </select>
+                </div>
+            </div> 
+            <button class="btn btn-info mt-2 showBtn" id="<?php echo $result[$k]["course_id"]; ?>">Show</button>
+            <!-- <button type="button" class="btn btn-info mt-2" data-toggle="modal" data-target="#modalQuickView${i}">Details</button> -->
+            <button type="button" class="btn btn-danger bbtn mt-2 store-item-icon">Apply now</button>
+
+        </div>
+    </div>
+
+
+
+
 <?php
     }
 }
--- page/our-courses.php
+++ page/our-courses.php
@@ -4,13 +4,14 @@ include_once "../Components/head.php";
 include_once "../CrudController.php";
 $crudcontroller = new CrudController();
 $result = $crudcontroller->readCourses();
+// $result = $crudcontroller->readCoursePrice();
 ?>
 
-    <div class="row">
+    <div class="row m-0 p-4">
         <h1>Course Overview</h1>
     </div>
 
-    <div class="row" id="container">
+    <div class="row p-4 m-0" id="container">
         <?php require_once "course_card.php" ?>
     </div>
 
